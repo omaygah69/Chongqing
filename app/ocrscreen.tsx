2765e24d2d0596b436dcddc80bd8788c3084161f
markjake <markjakearzadon@gmail.com>
2025-04-10 14:10:07 +0800

    tanginang project yan

--- app/ocrscreen.tsx
+++ app/ocrscreen.tsx
@@ -28,6 +28,18 @@ const compressImage = async (uri: string) => {
   }
 };
 
+const convertImageToBase64 = async (uri: string) => {
+  try {
+    const base64 = await FileSystem.readAsStringAsync(uri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+    return base64;
+  } catch (error) {
+    console.error("Error converting image to Base64:", error);
+    return null;
+  }
+};
+
 export default function OCRScreen() {
   const { imageUri }: { imageUri: any } = useLocalSearchParams();
   const [recognizedText, setRecognizedText] = useState<string | null>(null);
@@ -40,73 +52,81 @@ export default function OCRScreen() {
       setLoading(false);
       return;
     }
-    const processImage = async () => {
-      // Compress the image
-      const compressedImageUri = await compressImage(imageUri);
 
+    async function OCR(uri: string) {
+      const compressedImageUri = await compressImage(uri);
       if (!compressedImageUri) {
-        setErrorMessage("Failed to compress image");
-        setLoading(false);
-        return;
+        throw new Error("Compressed image URI is undefined");
+      }
+      const base64ImageUri = await convertImageToBase64(compressedImageUri);
+      const url = "https://api.ocr.space/parse/image";
+      let data = new FormData();
+
+      if (base64ImageUri) {
+        data.set("base64Image", `data:image/jpeg;base64,${base64ImageUri}`);
+      } else {
+        throw new Error("Failed to convert image to Base64");
       }
 
-      console.log(imageUri);
-      const formData = new FormData();
-
-      const localUri = compressedImageUri;
-      const filename = localUri.split("/").pop();
-      const type = "image/jpeg";
-
-      formData.append("apikey", "K86324583088957");
-      const file = {
-        uri: localUri,
-        name: filename,
-        type: type,
-      } as any;
-
-      formData.append("file", file);
+      data.set("apikey", "K86324583088957");
 
       try {
-        const response = await fetch("https://api.ocr.space/parse/image", {
-          method: "POST",
-          body: formData,
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        });
-
-        const data = await response.json();
-        console.log(data);
+        console.log("first");
+        const response = await fetch(url, { method: "POST", body: data });
+        const json = await response.json();
+        console.log(json);
       } catch (error) {
         console.error(error);
+        return { error: true };
       }
-    };
+    }
+    OCR(imageUri);
 
     // const processImage = async () => {
-    //   const worker = await Tesseract.createWorker("eng");
-    //   const {
-    //     data: { text },
-    //   } = await worker.recognize(imageUri);
-    //   setRecognizedText(text);
-    //   console.log(recognizedText);
-    //   await worker.terminate();
+    //   // Compress the image
+    //   const compressedImageUri = await compressImage(imageUri);
+
+    //   if (!compressedImageUri) {
+    //     setErrorMessage("Failed to compress image");
+    //     setLoading(false);
+    //     return;
+    //   }
+
+    //   console.log(imageUri);
+    //   const formData = new FormData();
+
+    //   const localUri = compressedImageUri;
+    //   const filename = localUri.split("/").pop();
+    //   const type = "jpg";
+
+    //   formData.append("apikey", "K86324583088957");
+    //   const file = {
+    //     uri: localUri,
+    //     name: filename,
+    //     type: type,
+    //   } as any;
+    //   console.log(formData);
+
+    //   formData.append("file", file);
+
+    //   try {
+    //     const response = await fetch("https://api.ocr.space/parse/image", {
+    //       method: "POST",
+    //       body: formData,
+    //       headers: {
+    //         "Content-Type": "multipart/form-data",
+    //       },
+    //     });
+
+    //     const data = await response.json();
+    //     console.log(data);
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
     // };
 
-    // try {
-    //   console.log("Processing image:", imageUri); // Debug log
-
-    //   Tesseract.recognize(imageUri, "eng", {
-    //     logger: (m) => console.log(m),
-    //   }).then(({ data: { text } }) => {
-    //     console.log("OCR Result:", text);
-    //     setRecognizedText(text || "No text recognized");
-    //   });
-    // } catch (error) {
-    //   console.error("OCR Error:", error);
-    //   setErrorMessage("Failed To Recognize Text.");
-    // }
-    setLoading(false);
-    processImage();
+    // setLoading(false);
+    // processImage();
   }, []);
 
   return (
